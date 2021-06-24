434ff03b0f0fd3b114f1bc87445f20c275d99fd5
maryamh <maryamsaleem63@yahoo.com>
2021-06-24 10:41:04 +0500

    login and signup ui

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,14 +12,46 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["react"],
+  ignorePatterns: ["node_modules", "src", "/build"],
+
   rules: {
+    "jsx-a11y/label-has-associated-control": [
+      "error",
+      {
+        required: {
+          some: ["nesting", "id"],
+        },
+      },
+    ],
+    "jsx-a11y/label-has-for": [
+      "error",
+      {
+        required: {
+          some: ["nesting", "id"],
+        },
+      },
+    ],
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        specialLink: ["to"],
+      },
+    ],
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
     "no-undef": "off",
     "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-    "no-unused-vars": [
-      "error",
-      { vars: "all", args: "after-used", ignoreRestSiblings: false },
-    ],
+    "no-unused-vars": "off",
+    "global-require": "off",
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        moduleDirectory: ["node_modules", "src/", "./"],
+      },
+    },
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./assets/css/tailwind.css";
 import App from "./App";
+import Login from "./Components/login";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
